b5f1c8585a8b7dedf21014276b867aa66e4f4721
LeandroDreary <l3androviegas@gmail.com>
2021-06-21 23:10:37 -0300

    v1.0.0

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Projeto Let Me Ask - Next Level Week Together
 
-Esse é um projeto de uma plataforma que oferece suporte a pessoas que precisam receber um grande número de perguntas e gostaria de organizar tudo isso. O projeto terá uma opção de dar like na pergunta para que assim a pessoa possa dar prioridade para perguntas com mais like visto que são as perguntas que mais interessa ao público:
+Esse é o projeto de uma plataforma que oferece suporte a pessoas que precisam receber um grande número de perguntas e gostaria de organiza-las. O projeto terá uma opção de dar like na pergunta para que assim a pessoa possa dar prioridade para perguntas com mais like visto que são as perguntas que mais interessa ao público:
 
 Funcionalidade:
 
---
+++ src/pages/NewRoom.tsx
@@ -0,0 +1,33 @@
+import IllustrationImg from '../assets/images/illustration.svg'
+import logoImage from '../assets/images/logo.svg'
+
+import '../styles/auth.scss'
+
+import { Button } from '../components/Button'
+import { Link } from 'react-router-dom'
+
+export function NewRoom() {
+    return (
+        <div id="page-auth">
+            <aside>
+                <img src={IllustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
+                <strong>Crie salas de Q&amp;A ao-vivo</strong>
+                <p>Tire as dúvidas da sua audiência em tempo-real.</p>
+            </aside>
+            <main>
+                <div className="main-content">
+                    <img src={logoImage} alt="Letmeask" />
+                    <h2>Criar uma nova sala</h2>
+                    <form>
+                        <input
+                            type="text"
+                            placeholder="Nome da Sala"
+                        />
+                        <Button type="submit">Criar Sala</Button>
+                    </form>
+                    <p>Quer entrar em uma sala existente? <Link to="/">Clique aqui</Link></p>
+                </div>
+            </main>
+        </div>
+    )
+}
